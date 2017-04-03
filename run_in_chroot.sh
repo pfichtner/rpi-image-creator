e798c27409ad716bc629b0e397433946406529b4
Peter Fichtner (pfichtner) <github.2012.07.fichtner@spamgourmet.com>
2017-04-03 08:24:09 +0200

    TODO added

--- run_in_chroot.sh
+++ run_in_chroot.sh
@@ -5,6 +5,9 @@ die() {
     exit 1
 }
 
+
+
+# TODO this all has not to run inside chroot!
 rm -f /etc/init.d/apply_noobs_os_config /etc/rc2.d/S01apply_noobs_os_config || die "Could not remove noobs stuff"
 rm -f /etc/profile.d/raspi-config.sh || die "Could not remove raspi-config first-boot stuff"
 
@@ -12,6 +15,21 @@ sed -i -e 's/^#\(FSCKFIX=\)no/\1yes/' /etc/default/rcS || die "Could not enable 
 echo "localhost" >/etc/hostname || die "Could not set hostname to localhost"
 echo "unset old_host_name # this will make the system always set the hostname from DHCP" >/etc/dhcp/dhclient-enter-hooks.d/unset_old_hostname || die "Could not apply fix http://blog.schlomo.schapiro.org/2013/11/setting-hostname-from-dhcp-in-debian.html"
 sed -i -e 's/^/#/' /etc/ld.so.preload || die "Could not disable ld.so.preload"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 apt-get -qq update || die "Could not update package sources"
 apt-get -qq dist-upgrade || die "Could not upgrade system"
